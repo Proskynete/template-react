ded2389b8e44def74d0bfef0895dc75392f6653c
Eduardo Alvarez <eduardo.a1993@gmail.com>
2017-03-20 11:07:19 -0300

    crear archivo 'webpack.config.js'

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  context: __dirname,
+  entry: {
+    app: ['./src/index.jsx']
+  },
+  output: {
+    path: '/assets/js/build',
+    filename: 'bundle.js',
+    publicPath: '/assets/js/build/'
+  },
+  devServer: {
+    contentBase: __dirname,
+    port: 9000,
+    compress:true,
+    inline: true
+  },
+  module: {
+    loaders: [
+      {
+        test: /(\.js|.jsx)$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'stage-2', 'react']
+        }
+      }
+    ]
+  }
+}
